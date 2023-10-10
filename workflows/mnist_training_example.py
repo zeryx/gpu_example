547371bf0bf13f6a40c56727ed99a1fb137f4321
zeryx <1892175+zeryx@users.noreply.github.com>
2023-10-10 12:57:46 -0400

    fixed to better leverage GPUs, and print correct statements
    Signed-off-by: zeryx <1892175+zeryx@users.noreply.github.com>

--- workflows/mnist_training_example.py
+++ workflows/mnist_training_example.py
@@ -1,23 +1,26 @@
 import torch
 from torch.utils.data import DataLoader
+from datetime import timedelta
 from torchvision import datasets, transforms
-from flytekit import task, workflow, Resources, ImageSpec
+from flytekit import task, workflow, Resources, ImageSpec, approve
 import torch as th
 from torch import nn
-
+from typing import Tuple
 """
 This example is a simple MNIST training example. It uses the PyTorch framework to train a simple CNN model on the MNIST dataset.
 The model is trained for 10 epochs and the validation loss is calculated on the test set.
 """
 
+# imagespec="ghcr.io/flyteorg/flytekit-python-template:mnist-training-latest"
+
 imagespec = ImageSpec(
-    name="flytekit",
-    registry="ghcr.io/zeryx",
-    base_image="ubuntu20.04",
-    requirements="requirements.txt",
-    cuda="11.2.2",
-    cudnn="8",
-    python_version="3.9"
+   name="flytekit",
+   registry="ghcr.io/zeryx",
+   base_image="ubuntu20.04",
+   requirements="requirements.txt",
+   cuda="11.2.2",
+   cudnn="8",
+   python_version="3.10"
 )
 
 
@@ -36,8 +39,9 @@ def get_dataset(training: bool, gpu: bool = False) -> DataLoader:
     return dataloader
 
 
-@task(requests=Resources(gpu="1", mem="10Gi", ephemeral_storage="10Gi", cpu="4"), container_image=imagespec)
-def train_gpu(dataset: DataLoader, n_epochs: int) -> th.nn.Sequential:
+@task(container_image=imagespec,
+      requests=Resources(cpu="2", mem="10Gi", ephemeral_storage="15Gi", gpu="1"))
+def train(dataset: DataLoader, n_epochs: int) -> th.nn.Sequential:
     """
     This task trains the model for the specified number of epochs.
     This variant of the task uses the GPU for training. as you can see from the Resources requested in the task decorator.
@@ -78,20 +82,21 @@ def train_model(model: th.nn.Sequential, optim: th.optim.Optimizer, dataset: Dat
     This function runs the inner training loop for the specified number of epochs.
     If a GPU is available, the model is moved to the GPU and the training is done on the GPU.
     """
-    if th.cuda.is_available():
-        model.to("cuda").train()
-    else:
-        model.train()
+    running_loss = 0.0
+    model = model.to("cuda").train()
     for epoch in range(n_epochs):
         for data, target in dataset:
             if th.cuda.is_available():
                 data, target = data.to("cuda"), target.to("cuda")
             optim.zero_grad()
             output = model.forward(data)
-            loss = th.nn.functional.nll_loss(output, target)
+            loss = th.nn.functional.cross_entropy(output, target)
             loss.backward()
             optim.step()
+            running_loss += loss.item()
+            print(f"Epoch {epoch + 1}/{n_epochs}, Loss: {running_loss / len(dataset)}")
     return model
+
 
 
 def get_model_architecture() -> (th.nn.Sequential, th.optim.Optimizer):
@@ -99,37 +104,26 @@ def get_model_architecture() -> (th.nn.Sequential, th.optim.Optimizer):
         nn.Conv2d(1, 32, kernel_size=3, padding=1),
         nn.ReLU(),
         nn.MaxPool2d(kernel_size=2),
-        nn.Conv2d(32, 1024, kernel_size=3, padding=1),
+        nn.Conv2d(32, 512, kernel_size=3, padding=1),
         nn.ReLU(),
         nn.MaxPool2d(kernel_size=2),
         nn.Flatten(),
-        nn.Linear(1024 * 7 * 7, 10024),
+        nn.Linear(512 * 7 * 7, 512),
         nn.ReLU(),
-        nn.Linear(10024, 10)
+        nn.Linear(512, 10)
     )
     optimizer = th.optim.SGD(model.parameters(), lr=0.003, momentum=0.9)
     return model, optimizer
 
 
 @workflow
-def mnist_workflow_gpu(n_epoch: int = 10) -> str:
+def mnist_workflow_gpu(n_epoch: int = 10) -> Tuple[str, th.nn.Sequential]:
     """
-    This workflow is identical to the previous one, except that it runs the training on the GPU.
+    This workflow is identical to CPU variant, except that it runs the training on the GPU.
     """
     training_dataset = get_dataset(training=True, gpu=True)
     test_dataset = get_dataset(training=False, gpu=True)
-    trained_model = train_gpu(dataset=training_dataset, n_epochs=n_epoch)
+    trained_model = train(dataset=training_dataset, n_epochs=n_epoch)
     output = validation_loss(model=trained_model, dataset=test_dataset)
-    return output
+    return output, trained_model
 
-
-@workflow
-def mnist_workflow_cpu(n_epoch: int = 10) -> str:
-    """
-    This workflow is identical to the previous one, except that it runs the training on the GPU.
-    """
-    training_dataset = get_dataset(training=True, gpu=False)
-    test_dataset = get_dataset(training=False, gpu=False)
-    trained_model = train_gpu(dataset=training_dataset, n_epochs=n_epoch).with_overrides(cpu="2", mem="10Gi", ephemeral_storage="15Gi", gpu="0")
-    output = validation_loss(model=trained_model, dataset=test_dataset)
-    return output
